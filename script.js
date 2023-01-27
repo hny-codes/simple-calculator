1e804fb38e44e0eed215c63c2e2178d0e01f1d5d
Henry <hnguyhenry@protonmail.com>
2023-01-27 13:17:48 -0800

    Add basic calculator operations

---
+++ script.js
@@ -0,0 +1,19 @@
+// Add operation
+const add = (a, b) => {
+  return a + b;
+};
+
+// Subtract operation
+const subtract = (a, b) => {
+  return a - b;
+};
+
+// Multiplication operation
+const multiply = (a, b) => {
+  return a * b;
+};
+
+// Division operation
+const division = (a, b) => {
+  return b === 0 ? "Math error" : a / b;
+};
