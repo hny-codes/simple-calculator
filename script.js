0117951356243204931ac09d615581d014ac3c66
Henry <hnguyhenry@protonmail.com>
2023-01-27 15:01:39 -0800

    Add functionality to calculator keys and operators

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Constants
+let totalDisplay = document.querySelector(".total-display").textContent;
+let currentDisplay = document.querySelector(".current-display").textContent;
+
+let firstNumber = 0;
+let secondNumber = 0;
+let op = "";
+
 // Add operation
 const add = (a, b) => {
   return a + b;
@@ -27,7 +35,7 @@ const operation = (op, a, b) => {
     case "-":
       return subtract(a, b);
       break;
-    case "*":
+    case "X":
       return multiply(a, b);
       break;
     case "/":
@@ -37,3 +45,75 @@ const operation = (op, a, b) => {
       return "Math error";
   }
 };
+
+// Full operation
+const performCalculation = () => {};
+
+// Add event listeners to display key contents when clicked
+const addKeys = () => {
+  const test = document.querySelector("#equal");
+  console.log(test.classList.contains("op"));
+
+  const keys = document.querySelectorAll(".key");
+  console.log(keys);
+  keys.forEach((key) => {
+    if (key.classList.contains("op")) {
+      key.addEventListener("click", () => {
+        if (firstNumber != 0) {
+          console.log(true);
+          secondNumber = document.querySelector(".current-display").textContent;
+          let results = operation(
+            op,
+            Number(firstNumber),
+            Number(secondNumber)
+          );
+          firstNumber = results;
+          op = key.children[0].textContent;
+          totalDisplay = firstNumber + op;
+          currentDisplay = "";
+          document.querySelector(".total-display").textContent = totalDisplay;
+
+        } else {
+          op = key.children[0].textContent;
+          firstNumber = currentDisplay;
+          currentDisplay = "";
+          totalDisplay = firstNumber + op;
+          document.querySelector(".total-display").textContent = totalDisplay;
+        }
+      });
+    } else if (key.classList.contains("clear")) {
+      key.addEventListener("click", () => {
+        op = "";
+        firstNumber = 0;
+        secondNumber = 0;
+        currentDisplay = 0;
+        totalDisplay = "";
+        document.querySelector(".total-display").textContent = totalDisplay;
+        document.querySelector(".current-display").textContent = currentDisplay;
+      });
+    } else if (key.classList.contains("equal")) {
+      key.addEventListener("click", () => {
+        secondNumber = document.querySelector(".current-display").textContent;
+        let results = operation(op, Number(firstNumber), Number(secondNumber));
+        document.querySelector(".current-display").textContent = results;
+        op = "";
+        firstNumber = 0;
+        secondNumber = 0;
+        currentDisplay = 0;
+        totalDisplay = "";
+        document.querySelector(".total-display").textContent = totalDisplay;
+      });
+    } else {
+      key.addEventListener("click", () => {
+        if (currentDisplay == 0) {
+          currentDisplay = "";
+        }
+        currentDisplay += key.children[0].textContent;
+        console.log(currentDisplay);
+        document.querySelector(".current-display").textContent = currentDisplay;
+      });
+    }
+  });
+};
+
+addKeys();
